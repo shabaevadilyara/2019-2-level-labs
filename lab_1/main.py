62648881dd86aa3df7314361cd9108dcd5a1576b
shabaevadilyara <dilyara.shabaeva@mail.ru>
2019-10-06 20:27:00 +0300

    Done

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,51 +1,66 @@
-text = "Nowadays solar energy is widely used as an alternative form of power. Solar panels transform the energy from the sun into electricity. The first plane that does not need fuel was constructed in France, in 2015. It uses only the sun's energy. The panels are placed on the huge wings of the plane. It doesn’t fly very fast. Solar energy can make the plane move at only 140 miles an hour. However, the plane is able to travel round the world. It is safe and can successfully cross areas of bad weather. In the future, engineers hope to construct a model that people can fly in. Our dream of environmentally friendly transport may come true very soon. Would you like to take a flight on the solar plane?"
-dict1 = {}
+text = ''
+freq_dict = {}
+
 
 def calculate_frequences(text):
-    text = text.lower()
-    alf = 'abcdefghijklmnopqrstuvwxyz '
-    for sym in text:
-        if sym not in alf: 
-            text = text.replace (sym, "")
-    text = text.split (' ')
-    for word in text:
-        if word not in dict1:
-            dict1 [word] = 1
-        else:
-            dict1 [word] = dict1 [word] +1 
+    freq_dict = {}
+    if type(text) is str and text != '':
+        text = text.lower()
+        for sym in text:
+            if sym not in 'abcdefghijklmnopqrstuvwxyz ':
+                text = text.replace(sym, "")
+        text = text.split(' ')
+        for word in text:
+            if word == '':
+                continue
+            elif word in freq_dict:
+                freq_dict[word] = freq_dict[word] + 1
+            else:
+                freq_dict[word] = 1
+        return freq_dict
+    else:
+        return freq_dict
 
 
-stop_words = ('an', 'a', 'the')
+stop_words = ()
 frequencies = {}
 
-def filter_stop_words (frequencies, stop_words):
-    for elm in dict1:
-        if elm not in stop_words:
-            frequencies [elm] = dict1 [elm]
-           
+
+def filter_stop_words(freq_dict, stop_words):
+    if freq_dict == {} or stop_words == () or stop_words is None:
+        return freq_dict
+    elif freq_dict is None and stop_words is None or freq_dict is None:
+        return {}
+    if freq_dict != {} and stop_words != ():
+        for elm in freq_dict.keys():
+             if elm not in stop_words and type(elm)is str:
+                frequencies[elm] = freq_dict[elm]
+        return frequencies
 
 
-top_n = 9
+top_n = 0
 toplist = []
+
+
 def get_top_n(frequencies, top_n):
     toplist = list(frequencies.items())
-    toplist.sort(key = lambda i: i[1])
-    toplist.reverse()
+    toplist.sort(key=lambda i: i[1], reverse=True)
     toplist1 = []
-    for elm in toplist:
-        toplist1 += elm
-        top_n -= 1
-        if top_n == 0:
-            break
-    for elm in toplist1:
-        if type(elm) is int:
-            toplist1.remove(elm)
-    toplist1 = tuple(toplist1)
-    print (toplist1)
-  
-    
+    if top_n > 0:
+        for elm in toplist:
+            toplist1 += elm
+            top_n -= 1
+            if top_n == 0:
+                break
+        for elm in toplist1:
+            if type(elm) is int:
+                toplist1.remove(elm)
+        toplist1 = tuple(toplist1)
+        return toplist1
+    else:
+        return()
 
 
 calculate_frequences(text)
-filter_stop_words (frequencies, stop_words)
-get_top_n (frequencies, top_n)
+filter_stop_words(frequencies, stop_words)
+get_top_n(frequencies, top_n)
